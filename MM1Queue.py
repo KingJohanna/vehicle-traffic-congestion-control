5f9c2fb266b5f3e9eb2ee859c8410fc9f9a163de
kingjohanna <johanna@LAPTOP-1K5QM5G7.localdomain>
2022-06-16 16:19:11 -0600

    M/M/1 queue implemented and used to model and simulate a two-way intersection.

--- MM1Queue.py
+++ MM1Queue.py
@@ -1,12 +1,12 @@
 from scipy.stats import poisson
 from scipy.stats import expon
 import random
+import queue
 
 class Queue:
     def __init__(self):
+        # self.vehicles = queue.Queue()
         self.queue_length = 0
-        self.departures = 0
-        self.arrivals = 0
         self.arrival_rate = 0 # vehicles per second
         self.departure_rate = 0 # vehicles per second
     
@@ -16,12 +16,10 @@ class Queue:
     
     def append(self) -> None:
         self.queue_length += 1
-        self.arrivals += 1
         
     def remove(self) -> None:
         if self.queue_length > 0:
             self.queue_length -= 1
-            self.departures += 1
             
 class QueueSimulator:
     def __init__(self):
@@ -29,6 +27,9 @@ class QueueSimulator:
         self.time = 0
         self.time_since_arrival = 0
         self.time_served = 0
+        self.departures = 0
+        self.arrivals = 0
+        self.tot_wait_time = 0
         
     def initialize(self, avg_arrival_time: float, avg_departure_time: float) -> None:
         self.queue.initialize(avg_arrival_time=avg_arrival_time, avg_departure_time=avg_arrival_time)
@@ -37,9 +38,10 @@ class QueueSimulator:
         self.time += delta_t
         self.time_since_arrival += delta_t
         self.time_served += delta_t
+        self.tot_wait_time += delta_t*self.queue.queue_length
         
     def arrival_probability(self) -> float:
-        return poissin.cdf(x=self.time_since_arrival, mu=self.queue.arrival_rate)
+        return poisson.cdf(k=self.time_since_arrival, mu=1/self.queue.arrival_rate)
         
     def departure_probability(self, saturation_rate: float) -> float:
         if saturation_rate == 0:
@@ -48,14 +50,19 @@ class QueueSimulator:
         
         return saturation_rate*expon.cdf(x=self.time_served, scale=1/self.queue.departure_rate)
 
-    def run_event(self, delta_t: float) -> None: # Check for vehicle arrivals/departures at current time, then execute time-step
+    def run_event(self, delta_t: float, saturation_rate: float) -> None: # Check for vehicle arrivals/departures at current time, then execute time-step
         if random.random() < self.arrival_probability():
-            self.queue.remove()
+            self.queue.append()
             self.time_since_arrival = 0
+            self.arrivals += 1
         
-        if random.random() < self.departure_probability():
+        if random.random() < self.departure_probability(saturation_rate=saturation_rate):
             self.queue.remove()
             self.time_served = 0
+            self.departures += 1
             
-        self.time_step()
+        self.time_step(delta_t=delta_t)
+        
+    def avg_wait_time(self) -> float: # Some inaccuracy since tot_wait_time includes wait times of undeparted vehicles
+        return self.tot_wait_time/self.departures
         
