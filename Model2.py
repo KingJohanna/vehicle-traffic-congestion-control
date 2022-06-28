777a982ac0099c5bb2414a389afa6529b84b8a03
kingjohanna <johanna@LAPTOP-1K5QM5G7.localdomain>
2022-06-28 14:30:47 -0700

    Implemented Model 1 & 2 through inheritance of a base model. Includes example simulations.

--- Model2.py
+++ Model2.py
@@ -1,1 +1,177 @@
-class SingleQueue
+import BaseModel
+import random
+import numpy as np
+import Vehicle
+import TrafficLight
+import math
+
+class SingleQueueSimulator(BaseModel.BaseQueueSimulator):
+    def arrival_probability(self, delta_t: float) -> float:
+        """
+        Returns the probability of an arrival occurring within a time-step.
+        
+        delta_t : float
+            Time-step size.
+        """
+        return delta_t*self.queue.arrival_rate
+        
+    def departure_probability(self, delta_t: float, saturation_rate) -> float:
+        """
+        Returns the probability of a departure occurring within a time-step.
+        
+        delta_t : float
+            Time-step size.
+        """
+        if saturation_rate == 0:
+            return 0
+        
+        return delta_t*self.queue.departure_rate
+    
+    def run_event(self, delta_t: float, saturation_rate: float) -> Vehicle.Vehicle:
+        """
+        Runs all events (arrivals/departures) given the current circumstances and elapses time.
+        
+        delta_t : float
+            The time-step size [s].
+        saturation_rate : float
+            The current saturation rate. Determined by an external traffic light.
+        """     
+        departing_vehicle = None
+        
+        if random.random() < self.arrival_probability(delta_t=delta_t):
+            arriving_vehicle = Vehicle.Vehicle()
+            arriving_vehicle.initialize(position=self.queue.tail_position, direction=self.queue.direction)
+            self.queue.append(arriving_vehicle)
+            self.time_since_arrival = 0
+            self.arrivals += [self.arrivals[-1]+1]
+        else:
+            self.arrivals += [self.arrivals[-1]]
+
+        if random.random() < self.departure_probability(delta_t=delta_t, saturation_rate=saturation_rate):
+            departing_vehicle = self.queue.remove()
+            if departing_vehicle != None:
+                self.time_served = 0
+                self.departures += [self.departures[-1]+1]
+                self.tot_wait_time += departing_vehicle.wait_time
+                departing_vehicle.accelerate()
+            else:
+                self.departures += [self.departures[-1]]
+        else:
+            self.departures += [self.departures[-1]]
+        
+        self.queue_length += [self.queue.queue_length]
+        self.time_step(delta_t=delta_t)
+        
+        return departing_vehicle
+
+class ConnectedQueueSimulator(BaseModel.BaseQueueSimulator):
+    def queue_vehicle(self, arriving_vehicle: Vehicle.Vehicle) -> None:
+        """
+        Adds a vehicle to the queue.
+        
+        arriving_vehicle: Vehicle.Vehicle
+            The vehicle to be added to the queue.
+        """
+        self.queue.append(arriving_vehicle)
+        self.time_since_arrival = 0
+        self.arrivals += [self.arrivals[-1]+1]
+        
+    def departure_probability(self, delta_t: float, saturation_rate) -> float:
+        """
+        Returns the probability of a departure occurring within a time-step.
+        
+        delta_t : float
+            Time-step size.
+        """
+        if saturation_rate == 0:
+            return 0
+        
+        return delta_t*self.queue.departure_rate
+        
+    def run_event(self, delta_t: float, saturation_rate: float) -> None:
+        """
+        Runs all events (departures) given the current circumstances and elapses time.
+        
+        delta_t: float
+            The time-step size [s].
+        saturation_rate : float
+            The current saturation rate. Determined by an external traffic light.
+        """ 
+        departing_vehicle = None
+        
+        if self.time_since_arrival > 0:
+            self.arrivals += [self.arrivals[-1]]
+        
+        if random.random() < self.departure_probability(delta_t=delta_t, saturation_rate=saturation_rate):
+            departing_vehicle = self.queue.remove()
+            if departing_vehicle != None:
+                self.time_served = 0
+                self.departures += [self.departures[-1]+1]
+                self.tot_wait_time += departing_vehicle.wait_time
+                departing_vehicle.accelerate()
+            else:
+                self.departures += [self.departures[-1]]
+        else:
+            self.departures += [self.departures[-1]]
+        
+        self.queue_length += [self.queue.queue_length]
+        self.time_step(delta_t=delta_t)
+        
+        return departing_vehicle
+    
+class FourWayIntersectionSimulator(BaseModel.FourWayIntersectionSimulator):
+    def __init__(self):
+        """
+        queue_n : ConnectedQueueSimulator
+            The northbound queue.
+        queue_w : ConnectedQueueSimulator
+            The westbound queue.
+        queue_s : ConnectedQueueSimulator
+            The southbound queue.
+        queue_e : ConnectedQueueSimulator
+            The eastbound queue.
+        traffic_light_ns : TrafficLight.SimpleTrafficLight
+            The traffic light controlling the north- and southbound queues.
+        traffic_light_ew : TrafficLight.SimpleTrafficLight
+            The traffic light controlling the east- and westbound queues.
+        position : (float, float)
+            The centerpoint of the intersection.
+        length : float
+            The width of the roads.
+        time : float
+            The current simulation time [s].
+        """
+        self.queue_n = ConnectedQueueSimulator()
+        self.queue_w = ConnectedQueueSimulator()
+        self.queue_s = ConnectedQueueSimulator()
+        self.queue_e = ConnectedQueueSimulator()
+        self.traffic_light_ns = None
+        self.traffic_light_ew = None
+        self.position = (0.,0.)
+        self.length = 0.
+        self.time = 0.
+        
+class IntersectionNetworkSimulator(BaseModel.IntersectionNetworkSimulator):
+    def __init__(self):
+        """
+        grid_dimensions : (int,int)
+            The grid dimensions of the intersection network.
+        grid_distance : float
+            The distance [m] between each intersection centerpoint.
+        intersections : [FourWayIntersectionSimulator]
+            The intersections contained in the network.
+        grid_inds : [(int,int)]
+            The grid indices within the network.
+        moving_vehicles : [Vehicle.Vehicle]
+            The vehicles within the network, not contained in any queue.
+        time : float
+            The current simulation time [s].
+        """
+        self.grid_dimensions = (0,0)
+        self.grid_distance = 0.
+        self.edge_type = SingleQueueSimulator
+        self.intersection_type = FourWayIntersectionSimulator
+        self.intersections = None
+        self.grid_inds = []
+        self.moving_vehicles = []
+        self.time = 0.
--- TrafficLight.py
+++ TrafficLight.py
@@ -1,4 +1,5 @@
 import numpy as np
+import random
 
 class SimpleTrafficLight:
     def __init__(self):
@@ -8,8 +9,9 @@ class SimpleTrafficLight:
         time_delay : float
             The time delay of the sequence. Light is green after after time_delay seconds has passed.
         """
-        self.period = 0
-        self.time_delay = 0
+        self.period = 0.
+        self.time_delay = 0.
+        self.time = 0.
         
     def initialize(self, period: float, time_delay: float) -> None:
         """
@@ -22,23 +24,121 @@ class SimpleTrafficLight:
         """
         self.period = period
         self.time_delay = time_delay
-        
-    def saturation_rate(self, t: float) -> float:
+    
+    def time_step(self, delta_t: float) -> None:
         """
-        Returns the saturation rate at a given time.
+        Elapses time by one time-step
+        """
+        self.time += delta_t
+        
+    def saturation_rate(self, delta_t=0.) -> float:
+        """
+        Returns the saturation rate for a given time.
         
         t : float
-            The current time [s]. 
+            The current time [s].
         """
-        if np.sin(2*np.pi*(t-self.time_delay)/self.period) > 0:
+        if np.sin(2*np.pi*(self.time-self.time_delay)/self.period) > 0:
             return 1
         else:
             return 0
         
-    def plot_green_light(self, plt, tot_time):
-        k = int(tot_time/self.period)
+    def plot_green_light(self, plt, end_time):
+        """
+        Plots the intervals when there was green light.
+        
+        plt : matplotlib.pyplot
+            The pyplot instance on which the plot will appear.
+        end_time : float
+            The end time of the plot.
+        """
+        k = int(end_time/self.period)
         
         for i in range(k):
             start = i*self.period+self.time_delay
             
             plt.axvspan(start, start+self.period/2, facecolor='g', alpha=0.2)
+            
+class MemoryLessTrafficLight:
+    def __init__(self):
+        """
+        service : bool
+            Determines whether the traffic light is at service or not.
+        green_to_red_rate : float
+            Probability of a green to red transition.
+        red_to_green_rate : float
+            Probability of a red to green transition.
+        time : float
+            The current simulation time.
+        """
+        self.service = bool(random.getrandbits(1))
+        self.green_to_red_probability = 0.
+        self.red_to_green_probability = 0.
+        
+    def initialize(self, green_to_red_rate: float, red_to_green_rate: float) -> None:
+        """
+        Initializes the SimpleTrafficLight instance.
+        
+        green_to_red_rate : float
+            Probability of a green to red transition.
+        red_to_green_rate : float
+            Probability of a red to green transition.
+        """
+        self.green_to_red_rate = green_to_red_rate
+        self.red_to_green_rate = red_to_green_rate
+        
+    def time_step(self, delta_t: float) -> None:
+        """
+        Elapses time by one time-step.
+        
+        delta_t : float
+            Time-step size
+        """
+        if self.service and random.random() < delta_t*self.green_to_red_rate:
+            self.service = False
+        elif not self.service and random.random() < delta_t*self.red_to_green_rate:
+            self.service = True
+            
+        self.time += delta_t
+            
+    def saturation_rate(self, delta_t=0.) -> float:
+        """
+        Returns the current saturation rate.
+        """
+        
+        return self.service
+    
+class MemoryLessTrafficLightMirror:
+    def __init__(self):
+        """
+        traffic_light : MemoryLessTrafficLight
+            The traffic light instance to mirror.
+        time : float
+            The current simulation time [s].
+        """
+        self.traffic_light = None
+        self.time = 0.
+        
+    def initialize(self, traffic_light: MemoryLessTrafficLight) -> None:
+        """
+        Initializes the MemoryLessTrafficLightMirror instance.
+        
+        traffic_light : MemoryLessTrafficLight
+            The traffic light instance to mirror.
+        """
+        self.traffic_light = traffic_light
+        
+    def time_step(self, delta_t: float) -> None:
+        """
+        Elapses time by one time-step.
+        
+        delta_t : float
+            The time-step size.
+        """
+        self.time += delta_t
+        
+    def saturation_rate(self, delta_t=0.):
+        """
+        Returns the current saturation rate.
+        """
+        return float(not self.traffic_light.service)
